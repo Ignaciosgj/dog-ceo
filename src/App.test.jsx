28dcf1244b9c296477bd8a0d7a35c4c4fbb65535
Jesus Ramos <jeenrade@gmail.com>
2023-02-01 13:09:02 -0300

    Deberia fallar

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,48 +1,55 @@
 import { render, screen } from "@testing-library/react";
 import axios from "axios";
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { App } from "./App";
 import { getAllBreeds } from "./helpers/getAllBreeds";
 
-vi.mock('axios')
-
-
+vi.mock("axios");
 
 describe("test", () => {
   it("render", () => {
     render(<App />);
   });
 
-//   it("Deberia Recibir la data", async () => {
-//     //arrange;
+  //   it("Deberia Recibir la data", async () => {
+  //     //arrange;
 
-//     //act
-//     const result = await getAllBreeds();
+  //     //act
+  //     const result = await getAllBreeds();
 
-//     //Assert
-//     expect(result.status).toBe('success');
-//   });
+  //     //Assert
+  //     expect(result.status).toBe('success');
+  //   });
 
-  it("debería traer 3 breeds", async () => {
-    //arrange
-    const breedsMock = {
-        "affenpinscher": [],
-        "african": [],
-        "airedale": []
-    }
-    axios.mockResolvedValue({
-        data: breedsMock
-    })
+  //   it("debería traer 3 breeds", async () => {
+  //     //arrange
+  //     const breedsMock = {
+  //       affenpinscher: [],
+  //       african: [],
+  //       airedale: [],
+  //     };
+  //     axios.mockResolvedValue({
+  //       data: breedsMock,
+  //     });
 
-    //act
+  //     //act
+  //     const result = await getAllBreeds();
+
+  //     //assert
+  //     expect(result).toEqual(breedsMock);
+  //   });
+
+  it("Deberia fallar", async () => {
+    //Arrange
+    // const asyncMock = vi.fn.mockRejectedValue(new Error("Rejected value"));
+    //vi.fn().mockRejectedValue(new Error('Async error'))
+    axios.get.mockRejectedValue({ status: "Error" });
+    //Act
+
     const result = await getAllBreeds();
+    console.log(result);
 
-    //assert
-    expect(result).toEqual(breedsMock);
+    //Assert
+    expect(result).toThrowError("Cuak");
   });
-
-  
 });
-
-
-
--- src/helpers/getAllBreeds.js
+++ src/helpers/getAllBreeds.js
@@ -1,7 +1,11 @@
-import axios from "axios"
+import axios from "axios";
 
 export const getAllBreeds = async () => {
-  const result = await axios('https://dog.ceo/api/breeds/list/all');
-  const  message  = result.data;
-  return message;
-}
+  try {
+    const result = await axios("https://dog.ceo/api/breeds/list/all");
+    const message = result.data;
+    return message;
+  } catch (error) {
+    throw new Error("Cuak");
+  }
+};
